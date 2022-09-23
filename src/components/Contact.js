c1869af226fbdbda5639d4812f5d8fd1a00ead7a
Aji Muhammad Sofwan <ajimsofwan@gmail.com>
2022-09-23 19:41:26 +0700

    initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,31 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
-
+import { Card, CardContent, Avatar, Typography, Grid } from "@mui/material"; 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+    <div>
+      {data.map((contact, i) => (
+        <Card key={i} variant="outlined" sx={{ display: "block", mb: 1 }}>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item md={2}>
+                <Avatar src={contact.photo} sx={{ width: 65, height: 65 }} />
+              </Grid>
+              <Grid item md={10}>
+                <Typography>{contact.name}</Typography>
+                <Typography color="text.secondary">{contact.phone}</Typography>
+                <Typography color="text.secondary">{contact.email}</Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      ))}    
+    </div>
+    );
 };
 
 export default Contact;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,14 +1,87 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  TextField,
+  Button,
+} from "@mui/material";
+
 // Form dapat dibuat dengan TextField
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
+const ContactForm = ({ addContact }) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [newContact, setNewContact] = useState({
+        name: "",
+        phone: "",
+        email: "",
+        photo: "",
+      });
+    
+      const inputHandler = (e) => {
+        setNewContact({ ...newContact, [e.target.name]: e.target.value });
+      };
+    
+      const submitHandler = (event) => {
+        event.preventDefault();
+        addContact(newContact);
+        setNewContact({ name: "", phone: "", email: "", photo: "" });
+      };
 
     return (
-        <> </>
+            <form onSubmit={submitHandler}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                <TextField
+                    id="outlined-basic fullwidth"
+                    label="Name *"
+                    variant="outlined"
+                    margin="normal"
+                    name="name"
+                    value={newContact.name}
+                    onChange={inputHandler}
+                    fullWidth
+                />
+                <TextField
+                    id="outlined-basic fullwidth"
+                    label="Phone *"
+                    variant="outlined"
+                    margin="normal"
+                    name="phone"
+                    value={newContact.phone}
+                    onChange={inputHandler}
+                    fullWidth
+                />
+                <TextField
+                    id="outlined-basic fullwidth"
+                    label="Email *"
+                    variant="outlined"
+                    margin="normal"
+                    name="email"
+                    value={newContact.email}
+                    onChange={inputHandler}
+                    fullWidth
+                />
+                <TextField
+                    id="outlined-basic fullwidth"
+                    label="Photo URL *"
+                    variant="outlined"
+                    margin="normal"
+                    name="photo"
+                    value={newContact.photo}
+                    onChange={inputHandler}
+                    fullWidth
+                />
+                </CardContent>
+                <CardActions>
+                <Button type="submit">Add New</Button>
+                </CardActions>
+            </Card>
+            </form>
     );
 }
 
